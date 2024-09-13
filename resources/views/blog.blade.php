2c0cfbea37a00b1e7db3f5d496b61b61331b8a58
Moh Raffi Nasrullah <mhmdraffi1002@gmail.com>
2024-09-13 15:47:46 +0700

    Initial commit for bmti project

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <article class="py-8 max-w-screen-md border-b border-gray-300 ">
+    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Kisah Ini Menjadi Kisah Ku</h2>
+    <div class="text-base text-gray-500">
+      <a href="#">Rafi Humam Nugraha</a> | 11 September 2024
+    </div>
+    <p class="my-4 font-light">Perjalanan Menemukan Diri
+        Setiap orang punya cerita yang unik. "Kisah ini menjadi kisahku" berarti pengalaman dan perjalanan hidup membentuk siapa kita hari ini. Dalam perjalanan ini, ada suka dan duka, namun semua itu memberikan pelajaran berharga.
+        Meski kadang terinspirasi oleh kisah orang lain, yang paling penting adalah bagaimana kita menjalani kisah kita sendiri. Setiap langkah, baik kecil maupun besar, membawa kita menuju versi terbaik diri kita.
+        Akhirnya, perjalanan inilah yang membuat kisah kita bermakna. Ini adalah kisahku, dan aku bangga menjalani setiap bagiannya.
+    </p>
+    <a href="" class="font-medium text-blue-500 hover:underline" >Read More &raquo;</a>
+  </article>
+  <article class="py-8 max-w-screen-md border-b border-gray-300 ">
+    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Anak Kecil dari Ambon</h2>
+    <div class="text-base text-gray-500">
+      <a href="#">Kelvin Fariz Rozy</a> | 12 September 2024
+    </div>
+    <p class="my-4 font-light">anak kecil dari ambon
+        SAnak-anak di Ambon, seperti halnya di daerah lain, adalah simbol harapan masa depan. Dikenal dengan keramahannya, mereka tumbuh di lingkungan yang kaya akan budaya dan tradisi Maluku. Dari kecil, mereka diajarkan nilai gotong royong, menghargai alam, serta menjunjung tinggi keberagaman.
+        Meski Ambon memiliki sejarah konflik, anak-anak di sana sekarang tumbuh di era perdamaian. Dengan akses yang semakin baik ke pendidikan dan teknologi, generasi muda ini memiliki peluang untuk berkembang dan berkontribusi lebih besar bagi daerah dan negara. Mereka adalah generasi yang membawa semangat baru untuk masa depan Ambon yang lebih cerah.
+    </p>
+    <a href="" class="font-medium text-blue-500 hover:underline" >Read More &raquo;</a>
+  </article>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Kelas abangda</h3>
+  </x-layout>
